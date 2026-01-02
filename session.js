215b9c5d19316895924d003f578806f3b91cbfec
senuraya <senurah30@gmail.com>
2026-01-02 22:01:23 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPROVerce~15S6YIZ2nE",
 PORT: process.env.PORT || "8000"
 };
